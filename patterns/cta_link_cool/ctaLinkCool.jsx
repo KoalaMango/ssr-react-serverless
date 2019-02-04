0e489e3eb2de871786e2545af97d5f435c5e75dc
Mirko Prpic <striko.mirko@gmail.com>
2019-02-04 12:34:26 +0000

    New pattern introduced - feature/cta_link_cool

---
+++ patterns/cta_link_cool/ctaLinkCool.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+const ctaLinkCool = (props) => {
+  return (
+    <div className="entry">
+      <h1>{ props.title }</h1>
+      <div class="body">
+        { props.body }
+      </div>
+    </div>
+    );
+};
+export default ctaLinkCool;
