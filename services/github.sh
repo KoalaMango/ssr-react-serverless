b559ecea051d3f0603e757b01bef031916bb8d4e
Mirko Prpic <striko.mirko@gmail.com>
2019-02-04 12:04:29 +0000

    Added just for test

--- services/github.sh
+++ services/github.sh
@@ -3,7 +3,7 @@
 # Create a new branch and push to origin
 git checkout -b $1
 git add patterns/
-git commit -m "Added new pattern - $1"
+git commit -m "New pattern introduced - $1"
 git push origin $1
-git checkout master
+git checkout feature/pattern-builder-poc
 git branch -D $1
